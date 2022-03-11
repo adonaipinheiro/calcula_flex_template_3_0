d1252807854079a46382133ce4fbfb37068c18a9
Heider Pinholi Lopes <heidertreinamentos@gmail.com>
2022-03-11 12:53:21 -0300

    Autenticacao com Firebase

---
+++ app/src/main/java/br/com/calculaflex/data/repository/UserRepositoryImpl.kt
@@ -0,0 +1,21 @@
+package br.com.calculaflex.data.repository
+
+import br.com.calculaflex.data.remote.datasource.UserRemoteDataSource
+import br.com.calculaflex.domain.entity.RequestState
+import br.com.calculaflex.domain.entity.User
+import br.com.calculaflex.domain.entity.UserLogin
+import br.com.calculaflex.domain.repository.UserRepository
+
+data class UserRepositoryImpl(
+    val userRemoteDataSource: UserRemoteDataSource
+) : UserRepository {
+
+    override suspend fun getUserLogged(): RequestState<User> {
+        return userRemoteDataSource.getUserLogged()
+    }
+
+    override suspend fun doLogin(userLogin: UserLogin): RequestState<User> {
+        return userRemoteDataSource.doLogin(userLogin)
+    }
+
+}
---
+++ app/src/main/java/br/com/calculaflex/presentation/base/auth/BaseAuthViewModel.kt
@@ -0,0 +1,23 @@
+package br.com.calculaflex.presentation.base.auth
+
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import br.com.calculaflex.domain.entity.RequestState
+import br.com.calculaflex.domain.entity.User
+import br.com.calculaflex.domain.usecases.GetUserLoggedUseCase
+import kotlinx.coroutines.launch
+
+class BaseAuthViewModel(
+    private val getUserLoggedUseCase: GetUserLoggedUseCase
+) : ViewModel() {
+
+
+    var userLogged = MutableLiveData<RequestState<User>>()
+
+    fun getUserLogged(){
+        viewModelScope.launch {
+            userLogged.value = getUserLoggedUseCase.getUserLogged()
+        }
+    }
+}
